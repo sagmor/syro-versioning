1dba3d451138142bc267cb1436fd7c88e2d139c7
Seba Gamboa <me@sagmor.com>
2015-11-11 11:13:51 -0300

    Add code climate coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,8 @@
-$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
-require 'syro/versioning'
+require "codeclimate-test-reporter"
+CodeClimate::TestReporter.start
 
 require 'rack/test'
 RSpec.configure { |c| c.include Rack::Test::Methods }
 
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+require 'syro/versioning'
